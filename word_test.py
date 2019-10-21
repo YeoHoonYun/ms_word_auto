20f074435ad923fd146b4218e4266a6b4b8c1090
YeoHoonYun <cjswo9207@gmail.com>
2019-10-21 22:52:11 +0900

    first commit

---
+++ word_test.py
@@ -0,0 +1,21 @@
+import json
+from pathlib import Path
+from docx import Document
+
+file_path = "result/"
+my_file = Path(file_path)
+with open("info_list.json","r", encoding="utf-8") as f:
+    info_list = json.loads(f.read())
+# document = Document()
+# if not my_file.is_file():
+#     document.save(file_path + 'base.docx')
+for info in info_list:
+    document = Document(file_path + 'work_doc.docx')
+    for p in document.paragraphs:
+        if "{{{name}}}" in p.text:
+            p.text = p.text.replace("{{{name}}}", info.get("name"))
+        elif "{{{num}}}" in p.text:
+            p.text = p.text.replace("{{{num}}}", info.get("num"))
+        elif "{{{sub}}}" in p.text:
+            p.text = p.text.replace("{{{sub}}}", info.get("sub"))
+        document.save(file_path + 'complete_%s.docx' % (info.get("num")))
